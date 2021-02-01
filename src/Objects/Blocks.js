739f802aeff64330cb10f6959843c9a43c6af37f
Flavio Silva <flavioandre.2001.9.7@gmail.com>
2021-02-01 22:12:40 +0000

    Limite y positions

--- src/Objects/Blocks.js
+++ src/Objects/Blocks.js
@@ -8,12 +8,12 @@ export default class Asteroid extends Phaser.Physics.Arcade.Sprite {
 	}
 
 	generate() {
-		const size = randomNumber(16, 32) / 32;
-		this.setScale(size);
+		const size = randomNumber(16, 64) / 32;
+		this.setScale(size, 1);
+		const margin = 10;
 
-		const minPosition = size + 150;
-		const x = randomNumber(size, GlobalConfigs.screen.width - size);
-		const y = randomNumber(minPosition, GlobalConfigs.screen.height - minPosition);
+		const x = randomNumber(size + margin, GlobalConfigs.screen.width - size - margin);
+		const y = GlobalConfigs.screen.middleHeight;
 		this.setPosition(x, y);
 
 		this.setImmovable(true);
